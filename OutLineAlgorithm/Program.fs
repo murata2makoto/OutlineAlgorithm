b165b2b79dfd527c1fffe8cb3d28ac86cb846d6e
murata2makoto <eb2mmrt@gmail.com>
2023-04-30 18:48:05 +0900

    printfn ""にして、System.Console.WriteLine()を追放。 テストデータを少し変更

--- OutLineAlgorithm/Program.fs
+++ OutLineAlgorithm/Program.fs
@@ -7,19 +7,18 @@ open CreateHedge
 let main argv =
     let test seq1 = 
         seq1 |> Seq.iter (fun x -> printf "%A " x)
-        System.Console.WriteLine()
+        printfn ""
         let tpSeq = createTokenOrParenthesisSeq seq1 (fun x -> x)
         for e in tpSeq do
             match e with 
             | StartParenthesis -> printf "("
             | EndParenthesis -> printf ")"
             | Token(e) -> printf "%A" e
-        System.Console.WriteLine()
+        printfn ""
         let hedge, _ = sequence2Hedge tpSeq
         for tr in hedge do
             printfn "%A" tr
-        System.Console.WriteLine()
-    seq {1;2;3;2;1;2} |> test 
+        printfn ""
     seq {1} |> test 
     seq {1;1} |> test 
     seq {1;1;1} |> test 
@@ -31,6 +30,7 @@ let main argv =
     seq {1;2;2;1} |> test 
     seq {1;2;2;1;2} |> test 
     seq {1;2;3;1;2} |> test 
+    seq {1;2;3;4;5;1;2} |> test 
     seq {1;2;3;4;3;4;3;4;3;4;1;2} |> test 
     seq {1;2;3;4;2;3;4;2;3;1} |> test 
     1
